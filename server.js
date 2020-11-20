543e867888c6ee1de2cefca7784a6fd040cdfc19
Mitesh Gangaramani <mitesh.gangaramani@rapidops.com>
2020-11-20 14:46:43 +0530

    added

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.NODE_PORT || 3001;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.info("in api now waiting");
+    setTimeout(() => {
+        console.info("response sent");
+        res.send('Hello World!');
+    }, 240000);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
